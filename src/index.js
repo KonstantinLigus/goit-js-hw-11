97498e3487b3a50874b4c230db134bedcbc0ed28
Konstantin Ligus <kostya1989com@gmail.com>
2022-06-06 22:47:32 +0300

    do else

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const URL = 'https://pixabay.com/api/';
+let page = 1;
 
 const refs = {
   searchInput: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
 };
-refs.loadMoreBtn.hidden = true;
-refs.searchInput.addEventListener('submit', onSubmitHandler);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreClick);
+refs.searchInput.addEventListener('submit', onSubmitClick);
 
-async function onSubmitHandler(event) {
+async function onSubmitClick(event) {
   event.preventDefault();
-  const query = event.target.elements.searchQuery.value;
+  page = 1;
+  refs.loadMoreBtn.style.display = 'none';
+  refs.gallery.innerHTML = '';
+  if (event.target.elements.searchQuery.value === '') {
+    return;
+  }
+  const query = event.target.elements.searchQuery.value.trim();
   try {
     const contentArr = await fetchContent(query);
+    refs.loadMoreBtn.style.display = 'block';
+    page = 1;
     if (contentArr.hits.length === 0) {
       return Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
@@ -26,8 +35,20 @@ async function onSubmitHandler(event) {
   }
 }
 
+async function onLoadMoreClick(event) {
+  try {
+    page += 1;
+    const contentArr = await fetchContent(
+      refs.searchInput.elements.searchQuery.value.trim()
+    );
+    renderGallery(contentArr);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 async function fetchContent(query) {
-  const param = `?key=27854076-b3a96c006ceb1c322db1c0d19&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const param = `?key=27854076-b3a96c006ceb1c322db1c0d19&q=${query}&image_type=photo&orientation=horizontal&safesearch=truekk&page=${page}&per_page=40`;
   const resp = await fetch(URL + param);
   return resp.json();
 }
@@ -48,22 +69,22 @@ function renderGallery(contentArr) {
     <div class="info">
       <p class="info-item">
         <b>Likes</b>
-        ${likes}
+        <br>${likes}
       </p>
       <p class="info-item">
         <b>Views</b>
-        ${views}
+        <br>${views}
       </p>
       <p class="info-item">
         <b>Comments</b>
-        ${comments}
+        <br>${comments}
       </p>
       <p class="info-item">
         <b>Downloads</b>
-        ${downloads}
+        <br>${downloads}
       </p>
     </div>
   </div>`;
   });
-  refs.gallery.innerHTML = markupContent.join(' ');
+  refs.gallery.insertAdjacentHTML('beforeend', markupContent.join(' '));
 }
