3b2f0e670e861feba6a3eddf192a08a96e7ab744
Konstantin Ligus <kostya1989com@gmail.com>
2022-06-08 21:09:55 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const URL = 'https://pixabay.com/api/';
 let page = 1;
@@ -24,8 +26,7 @@ async function onSubmitClick(event) {
   query = event.target.elements.searchQuery.value.trim();
   try {
     const data = await fetchData(query);
-    submitDataChecker(data);
-    renderGallery(data);
+    submitDataCheckerAndRender(data);
   } catch (error) {
     console.log(error);
   }
@@ -35,8 +36,7 @@ async function onLoadMoreClick(event) {
   page += 1;
   try {
     const data = await fetchData(query);
-    onLoadMoreDataChecker(data);
-    renderGallery(data);
+    onLoadMoreDataCheckerAndRender(data);
   } catch (error) {
     console.log(error);
   }
@@ -59,51 +59,63 @@ function renderGallery(data) {
       comments,
       downloads,
     } = obj;
-    return `<div class="photo-card">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-    <div class="info">
-      <p class="info-item">
-        <b>Likes</b>
-        <br>${likes}
-      </p>
-      <p class="info-item">
-        <b>Views</b>
-        <br>${views}
-      </p>
-      <p class="info-item">
-        <b>Comments</b>
-        <br>${comments}
-      </p>
-      <p class="info-item">
-        <b>Downloads</b>
-        <br>${downloads}
-      </p>
-    </div>
-  </div>`;
+    return `
+      <div class="photo-card">
+      <a href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      </a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+            <br>${likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+            <br>${views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+            <br>${comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+            <br>${downloads}
+          </p>
+        </div>
+      </div>
+  `;
   });
   refs.gallery.insertAdjacentHTML('beforeend', markupContent.join(' '));
 }
 
-function onLoadMoreDataChecker(data) {
-  totalPages = Math.ceil(data.totalHits / perPage);
-  if (page === totalPages) {
-    refs.loadMoreBtn.style.display = 'none';
-    return Notify.info(
-      "We're sorry, but you've reached the end of search results."
-    );
-  }
-  refs.loadMoreBtn.style.display = 'block';
-}
-function submitDataChecker(data) {
+function submitDataCheckerAndRender(data) {
   totalPages = Math.ceil(data.totalHits / perPage);
   if (data.hits.length === 0) {
     refs.loadMoreBtn.style.display = 'none';
-    return Notify.failure(
+    Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
+    return;
   }
   if (page === totalPages) {
-    return (refs.loadMoreBtn.style.display = 'none');
+    refs.loadMoreBtn.style.display = 'none';
+    renderGallery(data);
+    return;
   }
+  refs.loadMoreBtn.style.display = 'none';
+  Notify.info(`Hooray! We found ${data.totalHits} images.`);
+  renderGallery(data);
+  let gallery = new SimpleLightbox('.gallery a');
+  refs.loadMoreBtn.style.display = 'block';
+}
+
+function onLoadMoreDataCheckerAndRender(data) {
+  totalPages = Math.ceil(data.totalHits / perPage);
+  if (page === totalPages) {
+    refs.loadMoreBtn.style.display = 'none';
+    Notify.info("We're sorry, but you've reached the end of search results.");
+    return;
+  }
+  renderGallery(data);
   refs.loadMoreBtn.style.display = 'block';
 }
